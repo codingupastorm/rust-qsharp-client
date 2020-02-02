2ea1980140da206ed6527895423fa327d1c83502
Jordan Andrews <jordandrews@hotmail.com>
2020-02-02 20:47:56 +1100

    Understanding module structure and making public client interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,16 @@
-use std::collections::HashMap;
-use reqwest::Response;
+mod qsharp_http_client;
+use qsharp_http_client::QSharpHttpClient;
 
-pub struct QSharpHttpClient<'a> {
-    uri: &'a str
+pub struct QSharpClient<'a> {
+    http_client: &'a QSharpHttpClient<'a>
 }
 
-impl QSharpHttpClient<'_> {
+impl QSharpClient<'_> {
+ 
+    pub async fn compile_file(){
+        // TODO: Get text from a file, send to API.
 
-    pub async fn compile_snippet(&self, snippet: &str) -> Result<Response, Box<dyn std::error::Error>> {
-
-        let mut map = HashMap::new();
-        map.insert("code", snippet);
-
-        let post_url = format!("{}/Snippets/compile", self.uri);
-
-        let client = reqwest::Client::new();
-        let resp = client.post(&post_url)
-            .json(&map)
-            .send()
-            .await?;
-
-        Ok(resp)
+        // Return a struct that represents the whole response.
+        unimplemented!()
     }
-
-}
-
-#[tokio::test]
-async fn can_compile_code(){
-
-    // This relies on the HTTP IQ# server from https://github.com/microsoft/iqsharp to be running.
-
-    const CODE: &str = "operation HelloQ() : Unit
-    {
-        Message($\"Hello from quantum world!\"); 
-    }";
-
-    const URI: &str = "http://localhost:8888";
-
-    let client = QSharpHttpClient{uri: URI};
-    let result = client.compile_snippet(CODE).await;
-    assert!(result.is_ok());
 }
